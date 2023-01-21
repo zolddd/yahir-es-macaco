a232062ada24c2f5e4765193ee556b44089e041e
valie <isauravpr@gmail.com>
2023-01-21 13:57:22 -0600

    mka

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
 
   return (
     <div className="App">
-      <h1>yahir dev</h1>
+      <h1>YAHIR DEV</h1>
       <h2>yahir es macaco</h2>
-      <p>te quiero mucho como la cola de chuchooo y si viene un cerrucho..</p>
+      <p>te quiero mucho como la colaa de chuchooo y si viene un cerrucho..</p>
       <p>le digo que se aleje porque te quiero mucho</p>
       <p>muuuak</p>
     </div>
