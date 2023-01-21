32a3b377cdfe4d9ec1c412db087b24f2ec898ee2
valie <isauravpr@gmail.com>
2023-01-21 13:37:21 -0600

    darko dev

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <h1>yahir dev</h1>
       <h2>yahir es macaco</h2>
       <p>te quiero mucho como la cola de chucho y si viene un cerrucho..</p>
+      <p>le digo que se aleje porque te quiero mucho</p>
     </div>
   )
 }
